eae14acf5bffebcddbbfe6924b45b4b8099dbffc
Akash Kumar Shelke <96672834+Akashokshelke96@users.noreply.github.com>
2022-11-14 17:02:42 +0530

    Child Parent Relationship
    Learning Vue.js

---
+++ ChildComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>
+            Child Component
+        </h1>
+        <h3 v-for="user in users" :key="user.name">
+        {{user.name}} -- {{user.email}}</h3>
+      
+        <button v-on:click="updateTitle"> Update Parent Title</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ChildComponent',
+    props:[
+        'users'
+    ],
+    methods:{
+        updateTitle(){
+            this.$emit('changeTitle',"Parent Updated")
+        }
+    }
+
+}
+</script>
